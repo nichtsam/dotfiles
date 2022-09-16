a9b1b3f41a25250c2addef1e4b8be5f62b9b1715
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2022-09-16 23:11:23 +0800

    fix: avoid excessive brew install

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -2,7 +2,13 @@
 
 echo "\n<<< Starting Homebrew Setup >>>\n"
 
-# TODO: add condition to avoid excessive curl
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# check if brew exist 
+# https://stackoverflow.com/a/677212
+if command -v brew >/dev/null 2>&1; then
+  echo "brew exists, skipping install"
+else
+  echo "brew doesn't exist, continuing with install"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 brew bundle --verbose
