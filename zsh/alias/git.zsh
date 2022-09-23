ccb5f36d6f2a225b44e0c80e546b276d204c86b5
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2022-09-24 00:45:10 +0800

    fix: drop oh_my_zsh

---
+++ zsh/alias/git.zsh
@@ -0,0 +1,73 @@
+# Stage
+alias ga='git add'
+alias gaa='git add --all'
+
+# Commit
+alias gc='git commit -v'
+alias gc!='git commit -v --amend'
+alias gca='git commit -v -a'
+alias gca!='git commit -v -a --amend'
+alias gcam='git commit -a -m'
+alias gcmsg='git commit -m'
+
+# Branch
+alias gb='git branch'
+alias gba='git branch -a'
+alias gbd='git branch -d'
+alias gbda='git branch --no-color --merged | command grep -vE "^([+*]|\s*($(git_main_branch)|$(git_develop_branch))\s*$)" | command xargs git branch -d 2>/dev/null'
+alias gbD='git branch -D'
+alias gcb='git checkout -b'
+alias gcm='git checkout $(git_main_branch)'
+alias gco='git checkout'
+
+# Merge
+alias gm='git merge'
+alias gma='git merge --abort'
+
+# Rebase
+alias grb='git rebase'
+alias grba='git rebase --abort'
+alias grbc='git rebase --continue'
+alias grbi='git rebase -i'
+alias grbm='git rebase $(git_main_branch)'
+alias grbs='git rebase --skip'
+
+# Push
+alias gp='git push'
+alias gpd='git push --dry-run'
+alias gpf='git push --force-with-lease'
+alias gpf!='git push --force'
+alias gpoat='git push origin --all && git push origin --tags'
+alias gpsup='git push --set-upstream origin $(git_current_branch)'
+
+# Pull
+alias gup='git pull --rebase'
+alias gupv='git pull --rebase -v'
+
+# Status
+alias gsb='git status -sb'
+alias gss='git status -s'
+alias gst='git status'
+
+# Log
+alias glg='git log --stat'
+alias glgp='git log --stat -p'
+alias glgg='git log --graph'
+alias glgga='git log --graph --decorate --all'
+alias glgm='git log --graph --max-count=10'
+alias glo='git log --oneline --decorate'
+alias glol="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'"
+alias glols="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --stat"
+alias glod="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'"
+alias glods="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --date=short"
+alias glola="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --all"
+alias glog='git log --oneline --decorate --graph'
+alias gloga='git log --oneline --decorate --graph --all'
+
+# Misc
+alias gcf='git config --list'
+alias gcl='git clone --recurse-submodules'
+alias gclean='git clean -id'
+alias gd='git diff'
+alias grs='git restore'
+
