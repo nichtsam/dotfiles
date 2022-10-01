973be9a177f86461a24aea1d625a65b1818574cb
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2022-10-01 14:11:14 +0800

    style: organize plugins grouping
    and format files

--- nvim/lua/nichtsam/packer.lua
+++ nvim/lua/nichtsam/packer.lua
@@ -1,18 +1,18 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer..."
-  vim.cmd [[packadd packer.nvim]]
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -26,54 +26,52 @@ vim.cmd([[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
-  use "wbthomason/packer.nvim" -- Have packer manage itself
+	-- Fundamental
+	use("wbthomason/packer.nvim") -- Have packer manage itself
+	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
+	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
 
-  -- Theme
-  use 'folke/tokyonight.nvim' -- Tokyo Night theme for neovim
+	-- Theme
+	use("folke/tokyonight.nvim") -- Tokyo Night theme for neovim
 
-  -- Integrations
-  use 'wakatime/vim-wakatime' -- WakaTime Integration
+	-- Integrations
+	use("wakatime/vim-wakatime") -- WakaTime Integration
 
-  -- Completion Plugins
-  use "hrsh7th/nvim-cmp" -- The completion plugin
-  use "hrsh7th/cmp-buffer" -- Buffer completions
-  use "hrsh7th/cmp-path" -- Path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- Snippet completions
-  use "hrsh7th/cmp-nvim-lsp" -- LSP completions
+	-- Completion Plugins
+	use("hrsh7th/nvim-cmp") -- The completion plugin
+	use("hrsh7th/cmp-buffer") -- Buffer completions
+	use("hrsh7th/cmp-path") -- Path completions
+	use("hrsh7th/cmp-cmdline") -- cmdline completions
+	use("saadparwaiz1/cmp_luasnip") -- Snippet completions
+	use("hrsh7th/cmp-nvim-lsp") -- LSP completions
 
-  -- Snippets
-  use "L3MON4D3/LuaSnip" -- Snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+	-- Snippets
+	use("L3MON4D3/LuaSnip") -- Snippet engine
+	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 
-  -- LSP
-  use "neovim/nvim-lspconfig" -- Configurations for Nvim LSP
-  use "williamboman/mason.nvim" -- Easily install and manage LSP servers, DAP servers, linters, and formatters.
-  use "williamboman/mason-lspconfig.nvim" -- Makes it easier to use lspconfig with mason.nvim
-  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+	-- LSP
+	use("neovim/nvim-lspconfig") -- Configurations for Nvim LSP
+	use("williamboman/mason.nvim") -- Easily install and manage LSP servers, DAP servers, linters, and formatters.
+	use("williamboman/mason-lspconfig.nvim") -- Makes it easier to use lspconfig with mason.nvim
+	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
 
-  -- Misc
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
