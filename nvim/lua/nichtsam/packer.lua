d0eced8af3be54b4eed33007bc796a4e4347956b
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2022-10-01 21:49:44 +0800

    feat(neovim): add autopairs
    and format files

--- nvim/lua/nichtsam/packer.lua
+++ nvim/lua/nichtsam/packer.lua
@@ -80,6 +80,9 @@ return packer.startup(function(use)
 		end,
 	})
 
+	-- Autopairs
+	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
--- nvim/lua/nichtsam/treesitter.lua
+++ nvim/lua/nichtsam/treesitter.lua
@@ -13,6 +13,10 @@ require("nvim-treesitter.configs").setup({
 		"phpdoc", -- problematic
 	},
 
+	autopairs = {
+		enable = true,
+	},
+
 	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
 	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
