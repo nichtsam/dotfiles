ef0fc4f0aca4e5ade5aba2684f56ca2392bbfeb4
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2022-10-30 01:43:53 +0800

    fix: deprecated

--- nvim/lua/nichtsam/mason/lsp/settings/opts.lua
+++ nvim/lua/nichtsam/mason/lsp/settings/opts.lua
@@ -56,7 +56,7 @@ local function on_attach(client, bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 return {
 	capabilities = capabilities,
