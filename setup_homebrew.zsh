9e36d15a184c99ece392d827c780968f72e30476
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2023-01-25 18:52:41 +0800

    move exists function zshenv

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -2,9 +2,7 @@
 
 echo "\n<<< Starting Homebrew Setup >>>\n"
 
-# check if brew exist 
-# https://stackoverflow.com/a/677212
-if command -v brew >/dev/null 2>&1; then
+if exists brew; then
   echo "Brew exists, skipping install"
 else
   echo "Brew doesn't exist, continuing with install"
