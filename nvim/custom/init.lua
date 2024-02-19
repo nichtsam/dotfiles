d289c96fcac85c38a8749d6b4beecd21bbd36532
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2024-02-19 09:32:26 +0100

    a maybe fix to weird semanticTokensProvider issue
    no clue what causes it, it doesn't always happen.
    this solution was suggested, and issue stops occuring after this.
    so that's it.

---
+++ nvim/custom/init.lua
@@ -0,0 +1,8 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+})
