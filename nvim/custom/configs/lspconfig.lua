2687c920e8e667431d1295b522d8e8c67bf930f6
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2024-04-10 17:11:59 +0100

    enhance lspconfig.lua

--- nvim/custom/configs/lspconfig.lua
+++ nvim/custom/configs/lspconfig.lua
@@ -7,7 +7,19 @@ local servers = {
   -- Web
   "html",
   "cssls",
-  "tailwindcss",
+  {
+    "tailwindcss",
+    settings = {
+      tailwindCSS = {
+        experimental = {
+          classRegex = {
+            { "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+            { "cx\\(([^)]*)\\)",  "(?:'|\"|`)([^']*)(?:'|\"|`)" }
+          },
+        },
+      },
+    }
+  },
   "tsserver",
   -- Rust
   "rust_analyzer",
@@ -17,9 +29,18 @@ local servers = {
   "templ"
 }
 
+local base = {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
+  if type(lsp) == "string" then
+    local name = lsp
+    lspconfig[name].setup(base)
+  else
+    local name = table.remove(lsp, 1)
+    local config = lsp
+    lspconfig[name].setup(vim.tbl_deep_extend("force", base, config))
+  end
 end
