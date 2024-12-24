b6bab26327447efecf6b68325dcb90581bb32141
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2024-12-25 01:32:17 +0800

    floating diagnostics <leader>lf

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,8 +7,9 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map('n','<leader>fm', function()
-    require("conform").format({ async = true, lsp_fallback = true, })
-end, {desc = "Format"})
+map("n", "<leader>lf", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Show diagnostics under the cursor" })
+map("n", "<leader>fm", function()
+  require("conform").format { async = true, lsp_fallback = true }
+end, { desc = "Format" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")k
