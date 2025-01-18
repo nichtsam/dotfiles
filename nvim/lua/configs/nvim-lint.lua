5daa828d27538a3c1877203ced022b8022713852
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2025-01-18 15:27:46 +0100

    don't nag when eslint config not found
    pls don't

--- nvim/lua/configs/nvim-lint.lua
+++ nvim/lua/configs/nvim-lint.lua
@@ -1,16 +1,16 @@
-require('lint').linters_by_ft = {
+require("lint").linters_by_ft = {
   javascript = { "eslint" },
   javascriptreact = { "eslint" },
   typescript = { "eslint" },
   typescriptreact = { "eslint" },
 
-  go = { "golangcilint" }
+  go = { "golangcilint" },
 }
 
 local group = vim.api.nvim_create_augroup("Lint", { clear = true })
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   group = group,
   callback = function()
-    require("lint").try_lint()
+    require("lint").try_lint(nil, { ignore_errors = true })
   end,
 })
