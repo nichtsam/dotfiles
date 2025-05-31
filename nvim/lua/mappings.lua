0193c53bb91061bf7b8d8d89cd5e52acd94a58c0
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2025-06-01 00:03:54 +0200

    add lsp mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,7 +7,10 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map("n", "<leader>lf", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Show diagnostics under the cursor" })
+map("n", "<leader>gd", "gd", { desc = "LSP Go to definition" })
+map("n", "<leader>gD", "gD", { desc = "LSP Go to declaration" })
+map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+map("n", "<leader>lf", vim.diagnostic.open_float, { desc = "Show diagnostics under the cursor" })
 map("n", "<leader>fm", function()
   require("conform").format { async = true, lsp_fallback = true }
 end, { desc = "Format" })
