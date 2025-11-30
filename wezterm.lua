cbbbf78f8b3fdd8d9ad344f5fa725ace4e61457f
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2025-11-30 14:59:44 +0100

    wezterm add some movement key bindings

--- wezterm.lua
+++ wezterm.lua
@@ -1,4 +1,16 @@
-return {
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local config = {
 	color_scheme = "tokyonight-storm",
 	window_background_opacity = 0.8,
 }
+
+config.keys = {
+	{ key = "LeftArrow", mods = "OPT", action = act.SendString("\x1bb") }, -- backward-word
+	{ key = "RightArrow", mods = "OPT", action = act.SendString("\x1bf") }, -- forward-word
+	{ key = "LeftArrow", mods = "CMD", action = act.SendString("\x01") }, -- beginning-of-line
+	{ key = "RightArrow", mods = "CMD", action = act.SendString("\x05") }, -- end-of-line
+}
+
+return config
