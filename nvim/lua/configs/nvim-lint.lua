c93d6aeb0294ced78e45ce678576869d69ce654c
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2026-01-09 17:28:24 +0100

    clean a bit nvim lint

--- nvim/lua/configs/nvim-lint.lua
+++ nvim/lua/configs/nvim-lint.lua
@@ -1,41 +1,23 @@
 local nvlint = require "lint"
 
--- temp solution for switching linters between projects
-local function merge(...)
-  local result = {}
-  for _, t in ipairs { ... } do
-    for k, v in pairs(t) do
-      result[k] = v
-    end
-    local mt = getmetatable(t)
-    if mt then
-      setmetatable(result, mt)
-    end
-  end
-  return result
-end
+nvlint.linters_by_ft = {
 
-local eslint = {
   javascript = { "eslint" },
   javascriptreact = { "eslint" },
   typescript = { "eslint" },
   typescriptreact = { "eslint" },
-}
 
-local biome = {
-  javascript = { "biomejs" },
-  javascriptreact = { "biomejs" },
-  typescript = { "biomejs" },
-  typescriptreact = { "biomejs" },
-}
+  -- javascript = { "biomejs" },
+  -- javascriptreact = { "biomejs" },
+  -- typescript = { "biomejs" },
+  -- typescriptreact = { "biomejs" },
 
-nvlint.linters_by_ft = merge(eslint, {
   go = { "golangcilint" },
   solidity = { "solhint" },
   python = { "ruff" },
   yaml = { "vacuum" },
   json = { "vacuum" },
-})
+}
 
 local group = vim.api.nvim_create_augroup("Lint", { clear = true })
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
