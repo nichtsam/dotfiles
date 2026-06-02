deb81480031c907c0c39d62dc18e94dad6254740
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2026-06-02 12:59:26 +0200

    add keymap for navigating git hunks

--- nvim/lua/configs/gitsigns.lua
+++ nvim/lua/configs/gitsigns.lua
@@ -1,3 +1,29 @@
 return {
   current_line_blame = true,
+  on_attach = function(bufnr)
+    local gitsigns = require "gitsigns"
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map("n", "]c", function()
+      if vim.wo.diff then
+        vim.cmd.normal { "]c", bang = true }
+      else
+        gitsigns.nav_hunk "next"
+      end
+    end)
+
+    map("n", "[c", function()
+      if vim.wo.diff then
+        vim.cmd.normal { "[c", bang = true }
+      else
+        gitsigns.nav_hunk "prev"
+      end
+    end)
+  end,
 }
